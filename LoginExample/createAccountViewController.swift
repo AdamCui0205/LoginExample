69a59ebb1a30d67cd31fb8349a0b07ca849a4686
Peng Cui <cuipeng2323333@gmail.com>
2024-08-13 13:53:59 -0700

    signup and signin update

--- LoginExample/createAccountViewController.swift
+++ LoginExample/createAccountViewController.swift
@@ -6,16 +6,35 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class createAccountViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func signupClicked(_ sender: UIButton) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error {
+                print("error")
+            }
+            else {
+                // go to home screen
+                self.performSegue(withIdentifier: "goToNext", sender: self)
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
